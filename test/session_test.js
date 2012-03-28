3a00be5c1dc2bc87fa94889fd473d2425d1fecb3
Alexander Flatter <flatter@gmail.com>
2012-03-28 17:20:05 +0200

    session_test uses ClientMock.

--- test/session_test.js
+++ test/session_test.js
@@ -1,21 +1,23 @@
-/*global suite test*/
+/*global suite test setup*/
 
-var assert  = require('chai').assert
-   ,spy     = require('sinon').spy
-  , Session = require('webdriver/lib/session');
-
-var fakeClient = function() {
-  return {request: spy()};
-};
+var assert     = require('chai').assert
+  , wd         = require('./lib')
+  , Session    = wd.Session
+  , ClientMock = wd.ClientMock;
 
 suite('Session', function() {
-  var id     = 1
-    , client = {}
-    , caps   = {};
+  var id   = 1
+    , session, client;
+
+  setup(function() {
+    client = new ClientMock();
+  });
 
   suite('constructor', function() {
+    var caps = {};
+
     test('making a new instance using .create()', function() {
-      var session = Session.create({id: id, client: client, capabilities: caps});
+      session = Session.create({id: id, client: client, capabilities: caps});
 
       assert.equal(session.id,           id,     'id is set on the instance');
       assert.equal(session.client,       client, 'client is set on the instance');
@@ -25,7 +27,7 @@ suite('Session', function() {
     });
 
     test('making a new instance using the new keyword', function() {
-      var session = new Session({id: id, client: client, capabilities: caps});
+      session = new Session({id: id, client: client, capabilities: caps});
 
       assert.equal(session.id,           id,     'id is set on the instance');
       assert.equal(session.client,       client, 'client is set on the instance');
@@ -39,59 +41,53 @@ suite('Session', function() {
     var method   = 'GET'
       , resource = '/foo'
       , params   = null
-      , callback = function() {};
+      , callback = function() {}
+      , request;
 
     test('scopes all paths to the session', function() {
-      var request  = spy()
-        , client   = {request: request}
-        , session  = Session.create({id: id, client: client})
-        , args;
-
+      session = Session.create({id: id, client: client});
       session.request(method, resource, params, callback);
-      assert.ok(request.calledOnce, 'client receives one request');
 
-      args = request.getCall(0).args;
-      assert.equal(args[0], method,                      'method is not modified');
-      assert.equal(args[1], '/session/' + id + resource, 'resource is scoped');
-      assert.equal(args[2], params,                      'params are not modified');
-      assert.equal(args[3], callback,                    'callback is not modified');
+      assert.equal(client.callCount, 1, 'client receives one request');
+
+      request = client.lastRequest;
+      assert.equal(request.method,   method,                      'method is not modified');
+      assert.equal(request.resource, '/session/' + id + resource, 'resource is scoped');
+      assert.equal(request.params,   params,                      'params are not modified');
+      assert.equal(request.callback, callback,                    'callback is not modified');
     });
   }); // request
 
   suite('url', function() {
-    test('retrieves url when called without string', function() {
-      var client   = fakeClient()
-        , session  = Session.create({id: 1, client: client})
-        , callback = spy()
-        , args;
+    var callback = function() {}
+      , request;
 
+    test('retrieves url when called without string', function() {
+      session = Session.create({id: 1, client: client});
       session.url(callback);
 
-      assert.ok(client.request.calledOnce, 'client receives one request');
+      assert.equal(client.callCount, 1, 'client receives one request');
 
-      args = client.request.getCall(0).args;
-      assert.equal(args[0], 'GET',                       'method is not modified');
-      assert.equal(args[1], '/session/' + id + '/url',   'resource is scoped');
-      assert.equal(args[2], null,                        'params are not defined');
-      assert.equal(args[3], callback,                    'callback is passed');
+      request = client.lastRequest;
+      assert.equal(request.method,   'GET',                     'method is not modified');
+      assert.equal(request.resource, '/session/' + id + '/url', 'resource is scoped');
+      assert.equal(request.params,   null,                      'params are not defined');
+      assert.equal(request.callback, callback,                  'callback is passed');
     });
 
     test('navigates to url when called with a string', function() {
-      var client   = fakeClient()
-        , session  = Session.create({id: 1, client: client})
-        , callback = spy()
-        , url      = 'http://example.com'
-        , args;
+      var url = 'http://example.com';
 
+      session = Session.create({id: 1, client: client});
       session.url(url, callback);
 
-      assert.ok(client.request.calledOnce, 'client receives one request');
+      assert.equal(client.callCount, 1, 'client receives one request');
 
-      args = client.request.getCall(0).args;
-      assert.equal(args[0], 'POST',                      'method is not modified');
-      assert.equal(args[1], '/session/' + id + '/url',   'resource is scoped');
-      assert.deepEqual(args[2], {url: url},              'params includes url');
-      assert.equal(args[3], callback,                    'callback is passed');
+      request = client.lastRequest;
+      assert.equal(request.method,     'POST',                    'method is not modified');
+      assert.equal(request.resource,   '/session/' + id + '/url', 'resource is scoped');
+      assert.deepEqual(request.params, {url: url},                'params includes url');
+      assert.equal(request.callback,   callback,                  'callback is passed');
     });
   }); // url
 }); // Session
