61da0c75a6f30fdb277bdd64a19c7867fc6d530a
Alexander Flatter <flatter@gmail.com>
2012-04-01 08:34:56 -0700

    Move assertion helpers into their own module.

--- test/session_test.js
+++ test/session_test.js
@@ -1,44 +1,10 @@
 /*global suite test setup*/
 
-var assert     = require('chai').assert
-  , spy        = require('sinon').spy
+var spy        = require('sinon').spy
   , wd         = require('./lib')
+  , assert     = wd.assert
   , Session    = wd.Session
   , ClientMock = wd.ClientMock;
-
-assert.oneRequest = function(client, opts) {
-  assert.equal(client.callCount, 1, 'Only one request was made.');
-  assert.request(client.lastRequest, opts);
-};
-
-assert.request = function(request, opts) {
-  if (opts.method) {
-    assert.equal(request.method, opts.method);
-  }
-
-  if (opts.params) {
-    assert.deepEqual(request.params, opts.params);
-  }
-
-  if (opts.resource) {
-    assert.equal(request.resource, opts.resource);
-  }
-
-  if (opts.callback) {
-    assert.equal(request.callback, opts.callback);
-  }
-};
-
-assert.calledWithError = function(err, call) {
-  assert.equal(call.args[0], err, 'error is passed to the callback');
-  assert.isNull(call.args[1], 'callback does not receive a result');
-};
-
-assert.element = function(actual, session, id) {
-  assert.instanceOf(actual,    wd.Element, 'result is an Element');
-  assert.equal(actual.id,      id,         'element id is read from the response');
-  assert.equal(actual.session, session,    'the session passes itself');
-};
 
 suite('Session', function() {
   var id   = 1
